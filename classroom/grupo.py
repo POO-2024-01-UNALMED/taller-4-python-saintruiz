f1dd0de024b557db1d9f7b109b45b2cbfdfd1621
Santiago <saruizt@unal.edu.co>
2024-03-05 10:17:33 -0500

    Se hizo modificación al método agregarAlumno teniendo en consideración los tests

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,25 +3,20 @@ class Grupo:
     grado = "Grado 12"
 
     def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
-        self._grupo = grupo
-        if asignaturas==None:
-            asignaturas=[]
-        self._asignaturas = asignaturas
-        if estudiantes==None:
-            estudiantes=[]
-        self.listadoAlumnos = estudiantes
+        self._grupo= grupo
+        self._asignaturas= asignaturas or []
+        self.listadoAlumnos=estudiantes or []
 
     def listadoAsignaturas(self, **args):
-        if self._asignaturas==None:
-            self._asignaturas=[]
         for x in args.values():
             self._asignaturas.append(x)
 
     def agregarAlumno(self, alumno, lista=None):
-        if lista==None:
-            lista=[]
-        lista.append(alumno)
-        self.listadoAlumnos=lista
+        if type(lista) !=list:
+            self.listadoAlumnos.append(alumno)
+        else:
+            lista.append(alumno)
+            self.listadoAlumnos+=lista
 
     def __str__(self):
         return "Grupo de estudiantes: " + self._grupo
--- prueba.py
+++ prueba.py
@@ -3,5 +3,33 @@ from classroom.grupo import Grupo
 
 asignatura1 = Asignatura("Vision por Computador")
 asignatura2 = Asignatura("POO", "Salon 503B")
-print(asignatura1)
-print(asignatura2) 
+
+grupo1 = Grupo()
+grupo2 = Grupo("Grupo 32")
+grupo3 = Grupo("Grupo 23", [asignatura1])
+grupo4 = Grupo("Grupo 12", [asignatura1, asignatura2], ["Jaime", "David", "Oswaldo"])
+
+grupo1.agregarAlumno("Alumno10")
+grupo2.agregarAlumno("Alumno20")
+grupo3.agregarAlumno("Alumno30")
+grupo4.agregarAlumno("Alumno40")
+
+grupo1.agregarAlumno("Alumno11")
+grupo2.agregarAlumno("Alumno21")
+grupo3.agregarAlumno("Alumno31")
+grupo4.agregarAlumno("Alumno41")
+
+grupo1.agregarAlumno("Alumno14", ["Alumno12", "Alumno13"])
+grupo2.agregarAlumno("Alumno24", ["Alumno22", "Alumno23"])
+grupo3.agregarAlumno("Alumno34", ["Alumno32", "Alumno33"])
+grupo4.agregarAlumno("Alumno44", ["Alumno42", "Alumno43"])
+
+
+print(grupo1) #"Grupo de estudiantes: grupo predeterminado"
+print(grupo1.listadoAlumnos) #["Alumno10", "Alumno11", "Alumno12", "Alumno13", "Alumno14"]
+print(grupo2) #"Grupo de estudiantes: Grupo 32"
+print(grupo2.listadoAlumnos) #["Alumno20", "Alumno21", "Alumno22", "Alumno23", "Alumno24"]
+print(grupo3) #"Grupo de estudiantes: Grupo 23"
+print(grupo3.listadoAlumnos) #["Alumno30", "Alumno31", "Alumno32", "Alumno33", "Alumno34"]
+print(grupo4) #"Grupo de estudiantes: Grupo 12"
+print(grupo4.listadoAlumnos) #["Jaime", "David", "Oswaldo", "Alumno40", "Alumno41", "Alumno42", "Alumno43", "Alumno44"]
